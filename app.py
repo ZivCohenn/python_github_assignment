49c8a1c6d28c7c2ed8daf689487d2b76393e6ef4
Ziv Cohen <zivcohen@usf.edu>
2025-11-20 21:51:49 -0500

    Finalize app.py

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
+"""
+Study Time Tracker
+Script that asks the user for daily study hours and
+calculates weekly estimate
+"""
+
 print("Welcome to my Python program!")
+#User input
 hours = input("How many hours did you study today? ")
 
-#convert and calculate weekly hours
+#convert weekly hours with error handling
 try:
     hours = float(hours)
 except ValueError:
     print("Please enter a valid number")
     exit()
-#result
+#Calculation and result
 weekly_hours = hours * 7
 print(f"You are on track to study {weekly_hours:.1f} hours this week.")
